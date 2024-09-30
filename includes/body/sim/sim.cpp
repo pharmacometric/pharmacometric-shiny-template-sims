d5a787e350c90bf8773ccbae623c1f9880a38915
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-09-30 15:25:08 -0400

    Update sim.cpp

--- includes/body/sim/sim.cpp
+++ includes/body/sim/sim.cpp
@@ -18,30 +18,30 @@ $GLOBAL
 #define IPRED (CENT/V2)
 
 $PARAM @annotated
-TVVCL   	: 0.3040  : Individual Central Clearance (L/day)
-TVVV2   	: 3.5900  : Individual Central Volume of Distribution (L)
-TVVQ    	: 0.3190  : Individual Inter-compartmental Clearance (L/day)
-TVVV3   	: 2.5300  : Individual Peripheral Volume of Distribution (L)
-TVVKA   	: 0.1070  : Individual Absorption Constant (1/day)
-TVVF1 	  : 0.5350  : Individual Bioavailability F in logit space (.)
-BWE1      : 0.6180  : Body Weight Exponent on CL and Q
-BWE2      : 0.5500  : Body Weight Exponent on V2 and V3
+TVVCL   	: 5  : Individual Central Clearance (L/day)
+TVVV2   	: 10  : Individual Central Volume of Distribution (L)
+TVVQ    	: 4  : Individual Inter-compartmental Clearance (L/day)
+TVVV3   	: 1  : Individual Peripheral Volume of Distribution (L)
+TVVKA   	: 0.2  : Individual Absorption Constant (1/day)
+TVVF1 	  : 0.9  : Individual Bioavailability F in logit space (.)
+EXP1      : 0.75  : Body Weight Exponent on CL and Q
+EXP2      : 1.0  : Body Weight Exponent on V2 and V3
 WT        : 0       : Individual Baeline Body Weight
 
 $OMEGA @annotated @block
-ECL : 0.0727 : Random effect on CL
-EV2 : 0.0313 0.0390 : Random effect on V2
-EF1 : 0.0417 0.0771 0.4790 : Random effect on F1
+ECL : 0.09 : Random effect on CL
+EV2 : 0.05 : Random effect on V2
+EF1 : 0.2 : Random effect on F1
 
 $SIGMA @annotated
-PROP 	: 0.2010	: Proportional error
-ADD	  : 0.0711  : Additive error
+PROP 	: 0.01	: Proportional error
+ADD	  : 0.07  : Additive error
 
 $MAIN
-double TVCL = TVVCL*pow((WT/70),BWE1);
-double TVV2 = TVVV2*pow((WT/70),BWE2);
-double TVQ = TVVQ*pow((WT/70),BWE1);
-double TVV3 = TVVV3*pow((WT/70),BWE2);
+double TVCL = TVVCL*pow((WT/70),EXP1);
+double TVV2 = TVVV2*pow((WT/70),EXP2);
+double TVQ = TVVQ*pow((WT/70),EXP1);
+double TVV3 = TVVV3*pow((WT/70),EXP2);
 double TVKA = TVVKA;
 
 double CL = TVCL*exp(ETA(1));
