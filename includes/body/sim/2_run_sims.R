f67c90ea1259aec3d807d6f5e4e7019c4ebebb1e
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-10-14 17:38:49 -0400

    Create 2_run_sims.R

---
+++ includes/body/sim/2_run_sims.R
@@ -0,0 +1,39 @@
+#########################################################################################
+#########################################################################################
+##
+##  Document Path: pharmacometric-shiny-template-main/includes/body/sim/run_sims.R
+##
+##  R version 4.2.0 (2022-04-22)
+##
+#########################################################################################
+#########################################################################################
+
+observe({
+  if (GLOBAL$start.sim) {
+
+    updateSimStatus("Preparing data...")
+    dataa <- data01()
+
+
+    updateSimStatus("Running simulations...")
+    GLOBAL$lastsim <- NULL
+    GLOBAL$lastsim <- mod %>%
+      data_set(dataa) %>% # idata_set(exidata) %>% ev(amt = 100, ii = 24, addl = 10) %>%
+      mrgsim(
+        end = 7 * input$enddoseat,
+        obsonly = T,
+        delta = input$samplingfrequency,
+        carry_out = c("WT", "KOUT", "IC50", "Group2", "Dose"),
+        atol = 1e-6,
+        output = "df"
+      ) %>%
+      mutate(ID = paste0("ID ", ID)) %>%
+      left_join(dataa %>% select(Group, Group2) %>% distinct())
+
+    updateSimStatus("Simulations completed...")
+    GLOBAL$start.sim <- FALSE
+    GLOBAL$start.plot <- TRUE
+  }
+})
+
+
