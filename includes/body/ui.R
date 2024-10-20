01d6fe65d490f5a70e381cb3951127318ba79419
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-10-20 08:23:14 -0400

    Update ui.R

--- includes/body/ui.R
+++ includes/body/ui.R
@@ -21,7 +21,7 @@ body.main <- moveable(
       "This section allows initiation of the runs, edit of general parameters and monitoring of progress",
       tabs = list(
         tabEntry("Main",
-                 numericInput("samplesize","Number of participants (per arm)",100, width = "100%"),
+                 numericInput("samplesize","Number of participants (per arm)",3, width = "100%"),
                  numericInput("enddoseat","Treatment duration (wks)",30, width = "100%"),
                  numericInput("samplingfrequency","Sampling frequency (hr)",1, width = "100%"),
                  numericInput("simulationseed","Simulation seed",1320, width = "100%"),
