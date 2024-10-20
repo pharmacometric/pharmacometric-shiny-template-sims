e7681f0ea1281ad6715b63be4df4b0b1b373bc39
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-10-20 13:47:22 -0400

    Update ui.R

--- includes/header/ui.R
+++ includes/header/ui.R
@@ -1,4 +1,5 @@
 header.main <- titlePanel(tags$div(
+  tags$img(src="logo.jpg")," ",
   "Simulation of XYZ Profiles Based on ABC Model",
   tags$div(class = "hidden-mobile hidden-tablet pull-right",
              actionButton("aboutproject", "", icon = icon("question")))
