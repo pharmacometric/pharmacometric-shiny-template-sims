e21d41ad5a162e7567ccc9c7a6dd7f0e6ee7feae
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-10-20 21:43:11 -0400

    Update 3_process_result.R

--- includes/body/sim/3_process_result.R
+++ includes/body/sim/3_process_result.R
@@ -10,21 +10,23 @@
 #########################################################################################
 
 data01_all <- reactive({
-  dataa <- data01()
+  dataa <- GLOBAL$lastsim
   if (nrow(dataa)) {
-    dataa %>% mutate(type = "all") %>% group_by(type, time) %>%
+    dataa %>% mutate(Group = "all") %>% group_by(Group, time) %>%
       summarise(
         IPRED_mean = mean(IPRED),
         IPRED_med = median(IPRED),
         sd = sd(IPRED),
         sem = sd(IPRED)/sqrt(length((IPRED))),
         q95 = quantile(IPRED,probs = 0.95),
-        q05 = quantile(IPRED,probs = 0.05))
+        q05 = quantile(IPRED,probs = 0.05),
+        q975 = quantile(IPRED,probs = 0.975),
+        q025 = quantile(IPRED,probs = 0.025))
   }
 })
 
 data02_group <- reactive({
-  dataa <- data01()
+  dataa <- GLOBAL$lastsim
   if (nrow(dataa)) {
     dataa %>% group_by(Group,Group2, time) %>%
       summarise(
@@ -33,6 +35,8 @@ data02_group <- reactive({
         sd = sd(IPRED),
         sem = sd(IPRED)/sqrt(length((IPRED))),
         q95 = quantile(IPRED,probs = 0.95),
-        q05 = quantile(IPRED,probs = 0.05))
+        q05 = quantile(IPRED,probs = 0.05),
+        q975 = quantile(IPRED,probs = 0.975),
+        q025 = quantile(IPRED,probs = 0.025))
   }
 })
