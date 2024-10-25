95cc1f75736fbf4842b8e5341ba33bdd7f2a45ef
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-10-24 21:54:12 -0400

    Update 4_summ_table.R

--- includes/body/sim/4_summ_table.R
+++ includes/body/sim/4_summ_table.R
@@ -1,16 +1,49 @@
-
 summar01 <- reactive({
-data.frame(
-  Group = 0,
-  AUC = 0,
-  Ctrough = 0,
-  Cmax = 0
-  )
+  dataa <- GLOBAL$lastsim
+  if (length(dataa) & not.null(dataa)) {
+    if (nrow(dataa)) {
+      print(names(dataa))
+      return(dataa %>%
+        filter(between(time,input$selectedrangesumm[1], input$selectedrangesumm[2])) %>%
+        group_by(Group, ID) %>%
+        summary(
+          AUC = calculate_auc(time, IPRED),
+          Ctrough = min(IPRED),
+          Cmax = max(IPRED)
+        ))
+    }
+  }
 
+  data.frame(
+    Group = "A",
+    ID = 1,
+    AUC = 0,
+    Ctrough = 0,
+    Cmax = 0
+  )
 })
 
-output$summaryexptbl <- renderDT(
-  summar01(), options = list(lengthChange = FALSE)
+
+summar02 <- reactive({
+  dataa <- summar01()
+
+  if (length(dataa)) {
+    if (nrow(dataa)) {
+      return(table1(~ AUC + Ctrough + Cmax | as.factor(Group), data = dataa))
+    }
+  }
+
+  data.frame(
+    Group = 1,
+    AUC = 0,
+    Ctrough = 0,
+    Cmax = 0
+  )
+})
+
+output$summaryexptbl <- renderTable(
+  summar02(),
+  options = list(lengthChange = FALSE, dom = "t")
 )
 
 
@@ -19,9 +52,7 @@ observe({
   updateSliderInput(
     session = session,
     "selectedrangesumm",
-              "Select range for summary",
+    "Select range for summary",
     value = c(0, input$enddoseat * 7), max = input$enddoseat * 7
   )
-
-
 })
