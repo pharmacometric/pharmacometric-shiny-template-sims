be3ab035f0a0155e7529e70a7086a52ad96fb819
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-10-25 22:36:27 -0400

    Update libs.R

--- includes/body/libs.R
+++ includes/body/libs.R
@@ -1,8 +1,9 @@
+set.seed(number(1))
 regimenDT = data.frame(
   Group = rep(LETTERS[1:3],each = 2),
-  Dose = rep(sample(c(50,10,100,200),2),3),
-  Frequency = rep(sample(1:3,2),3),
-  Additional = 3,
+  Dose = sample(seq(10,500,50),6),
+  Frequency = sample(1:5,6, replace = T),
+  Additional = sample(0:4,6, replace = T),
   Route = rep(sample(c("IV","SC")),3),
   WT = 70,
   stringsAsFactors = FALSE
