b0d2ea8254c58d1f83b2e37f3a0824d60c5ec306
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-10-25 22:28:48 -0400

    Update libs.R

--- includes/body/libs.R
+++ includes/body/libs.R
@@ -1,6 +1,6 @@
 regimenDT = data.frame(
   Group = rep(LETTERS[1:3],each = 2),
-  Dose = rep(sample(c(50,10)),3),
+  Dose = rep(sample(c(50,10,100,200),2),3),
   Frequency = rep(sample(1:3,2),3),
   Additional = 3,
   Route = rep(sample(c("IV","SC")),3),
