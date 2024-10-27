9b8543f35342ea8fa366c744f4b24ef7819f4041
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-10-27 15:08:31 -0400

    Update server.R

--- includes/header/server.R
+++ includes/header/server.R
@@ -1,8 +1,7 @@
-print("just here...server...header")
-print(this.path)
+message(this.path)
 observeEvent(input$aboutproject, {
   showModal(modalDialog(
     title = "About this app",
-    "This simulation app was created by Bill. 2024. Use  freely."
+    "This simulation app was created by William 2024. Use freely."
   ))
 })
