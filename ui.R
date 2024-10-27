4793738e34ff598c11938726e96aa7c04cab48a3
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-10-27 15:37:20 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,13 +1,37 @@
+############################################################################
+############################################################################
+##
+##  NO NEED TO ALTER THIS PAGE
+##
+##  Document: ui.R
+##
+##  Description: Assemble all UI elements from the "includes" folder
+##
+##  R version 4.4.1 (2024-06-14 ucrt)
+##
+#############################################################################
+#############################################################################
+
+
+
+# include all required UI files for head, body and footer
 for (ui_each in c(
   "includes/header",
   "includes/body",
   "includes/footer"
 )) {
   this.path = ui_each
-  source(file.path(ui_each,"ui.R"), local = T)
+  this.files = list.files(ui_each,pattern ="^ui\\.",
+             full.names = TRUE,
+             recursive = FALSE)
+  for(uifile in this.files)
+    source(uifile, local = T)
 }
 
 
+
+
+# create the main UI for the app
 ui <- fluidPage(
   useShinyjs(),
   use.cardpro(theme = "a"),
