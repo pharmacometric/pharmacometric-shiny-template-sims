56e964ab9065c9db22a9f9821c57ba1186fce921
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-10-27 15:37:18 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,4 +1,22 @@
+############################################################################
+############################################################################
+##
+##  NO NEED TO ALTER THIS PAGE
+##
+##  Document: server.R
+##
+##  Description: Assemble all server functions from the "includes" folder
+##
+##  R version 4.4.1 (2024-06-14 ucrt)
+##
+#############################################################################
+#############################################################################
+
+
 server <- function(input, output, session) {
+
+  # include all required server files for head, body and footer
+
   for (server_each in c(
     "includes/header",
     "includes/body",
@@ -10,6 +28,4 @@ server <- function(input, output, session) {
       input, output, session
     )
   }
-
-
 }
