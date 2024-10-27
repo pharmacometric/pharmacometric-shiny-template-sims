0a8577a62075676ac78eee7d847dc5e8b8a426c8
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-10-27 15:37:08 -0400

    Update server.R

--- includes/body/server.R
+++ includes/body/server.R
@@ -1,7 +1,22 @@
+############################################################################
+############################################################################
+##  Document Path: includes/body/server.R
+##
+##  Description: Server function for body
+##
+##  R version 4.4.1 (2024-06-14 ucrt)
+##
+#############################################################################
+#############################################################################
+
+# include all files required for simulations
+
 for(e in list.files(file.path(this.path,"sim/"), pattern = ".R$"))
   source(file.path(this.path,"sim", e), local = TRUE)
 
 
+
+# other outputs and event listeners for the body section
 
 output$tracksimulations <- renderText({
   if(is.null(GLOBAL$lastsim)){
@@ -9,14 +24,11 @@ output$tracksimulations <- renderText({
   }
 })
 
-
-
 observeEvent(input$runsimbutton,{
   disableSims()
   #Sys.sleep(2)
   GLOBAL$start.sim <- TRUE
 })
-
 
 output$summaryrestbl = renderDT(
   data01() %>% select(Group,ID,time,WT,Dose,cmt,ii,addl), options = list(lengthChange = FALSE)#,dom = 't'
@@ -25,8 +37,6 @@ output$summaryrestbl = renderDT(
 output$rawrestbl = renderDT(
   summary01(), options = list(lengthChange = FALSE), filter = list(position = "top")
 )
-
-
 
 output$rhstable1 <- renderRHandsontable({
   rhandsontable(regimenDT, width = "100%") %>%
